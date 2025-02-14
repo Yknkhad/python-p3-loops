ea2de2022e9fdb91e34d198fc50d8bba28e05559
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2025-02-14 09:37:08 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0 :
+        print (counter)
+        counter -= 1
+    print("Happy New Year!")
+
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
-    pass
+
+
+def fizzbuzz(num=None):
+    if num is None:
+        fizzbuzz_printer()
+    elif num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+
+
+    
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))  
